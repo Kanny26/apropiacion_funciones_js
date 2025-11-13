4c9281a628738e65147a0f1ef4c3f0151d700462
stephany moreno <stephanymoreno182@gmail.com>
2025-11-13 13:51:21 -0500

    Se agregaron los archivos que importan las funciones de los modulos

---
+++ app1.js
@@ -0,0 +1,9 @@
+//se importa la funcion que esta dentro de la carpeta modulos y el archivo
+import {saludoPersonalizado} from './modulos/1_saludo.js';
+//se piden los datos al usuario
+let nomb = prompt("Ingrese su nombre: ");
+let edad = prompt("Ingrese su edad: ");
+let ficha= prompt ("Ingrese su ficha: ");
+
+//se muestran los datos, llamando la funcion con los argumentos que recibio
+alert(saludoPersonalizado(nomb,edad,ficha));
